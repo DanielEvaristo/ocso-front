14deb73d0643a1efcfd03130b081a227d7d81b57
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-10-12 22:59:56 -0600

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,21 @@
+import { Input, Button } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function LoginPage() {
+    return (
+        <div className="bg-orange-500 px-10 py-2 rounded-md">
+            <p className="text-3xl my-4 text-white text-center">Iniciar Sesión <span></span></p>
+            <div className="flex flex-col gap-2 my-4 items-center">
+                <Input label="Email" type="email" isRequired={true} size="sm" />
+                <Input label="Contraseña" type="password" isRequired={true} size="sm" />
+            </div>
+            <div className="flex flex-col items-center gap-2">
+                <Button color="primary">Registrarse</Button>
+                <p className="text-white">
+                    ¿No tienes cuenta?{" "}
+                    <Link href="/signup" className="text-red-600 underline">Registrate</Link>
+                </p>
+            </div>
+        </div>
+    );
+}
