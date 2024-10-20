fddbaa07d46e634126bd7aaab0d9579004e330cb
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-10-20 05:59:22 -0600

    Form Create Location

--- app/dashboard/@locations/_components/FormNewLocation.tsx
+++ app/dashboard/@locations/_components/FormNewLocation.tsx
@@ -1,12 +1,25 @@
 import { createLocation } from "@/actions/locations/create";
-import { Input } from "@nextui-org/react";
+import { API_URL, TOKEN_NAME } from "@/constants";
+import { Button, Input } from "@nextui-org/react";
+import axios from "axios";
+import { cookies } from "next/headers";
+import SelectManager from "./SelectManager";
 
-export default function FormNewLocation(){
-
+export default async function FormNewLocation(){
+    const token = cookies().get(TOKEN_NAME)?.value;
+    const {data} = await axios.get(`${API_URL}/managers`,{
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    })
     return(
         <form action={createLocation}>
-            <Input label="Nombre de tienda" name="locationName"/>
-            <button>Subir</button>
+            <Input label="Nombre" name="locationName"/>
+            <Input label="Dirección" name="locationAddress"/>
+            <Input label="Latitud" name="locationLat"/>
+            <Input label="Longitud" name="locationLng"/>
+            <SelectManager managers={data}/>
+            <Button type="submit">Subir</Button>
         </form>
     )
 }
