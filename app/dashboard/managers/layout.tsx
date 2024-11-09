082fa3513eeb7a47c50892103ca01f0c2101a67c
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-11-09 17:56:43 -0600

    Nuevos stats

--- app/dashboard/managers/layout.tsx
+++ app/dashboard/managers/layout.tsx
@@ -8,8 +8,13 @@ export default function LayoutManager({children, count}:{children:ReactNode, cou
         <div className="w-4/12 max-h-[90vh] h-[90vh] overflow-hidden overflow-y-auto" >
             <ManagerCards/>
         </div>
-        <div className="w-7/12">
-            {children} {count}
+        <div className="w-7/12 flex flex-col items-center gap-10">
+            <div>
+                {children}
+            </div>
+            <div>
+                {count}
+            </div>
         </div>
         </>
     )
