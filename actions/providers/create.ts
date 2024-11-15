c519acd2b2987c4af87134625ad7ed3c189d26d9
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-11-15 17:53:22 -0600

    Actualizar provider

--- actions/providers/create.ts
+++ actions/providers/create.ts
@@ -5,13 +5,13 @@ import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
 
 export default async function createProvider(formData: FormData) {
-  let manager: any = {};
+  let provider: any = {};
   for (const key of formData.keys()) {
-    manager[key] = formData.get(key);
+    provider[key] = formData.get(key);
   }
   const response = await fetch(`${API_URL}/providers`, {
     method: "POST",
-    body: JSON.stringify(manager),
+    body: JSON.stringify(provider),
     headers: { ...authHeaders(),
     'content-type': 'application/json'
      },
