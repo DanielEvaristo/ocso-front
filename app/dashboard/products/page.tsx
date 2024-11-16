ebda2149c82bb9213abff2a8713851f9da71e3b3
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-11-16 04:42:50 -0600

    Filtrar por Proveedor

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -3,7 +3,7 @@ import { Product } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
 import FilteredCard from "./_components/FilteredCard";
 const ProductsPage = async () => {
-  const response = await fetch(`${API_URL}/products`, { 
+  const responseProducts = await fetch(`${API_URL}/products`, { 
     headers:{
       ...authHeaders()
     },
@@ -11,11 +11,20 @@ const ProductsPage = async () => {
       tags: ["dashboard:products"]
     }
   })
-  const products: Product[] = await response.json() 
+  const products: Product[] = await responseProducts.json() 
+  const responseProviders = await fetch(`${API_URL}/providers`, {
+    headers:{
+      ...authHeaders()
+    },
+    next:{
+      tags: ["dashboard:providers"]
+    }
+  })
+  const providers = await responseProviders.json()
   return (
     <div className="h-[90vh] w-full">
       <div className="w-3/12">
-        <FilteredCard products={products}/>
+        <FilteredCard products={products} providers={providers}/>
       </div>
     </div>
   )
