b8bf36224a35e035e59fa2a90d27051d30f3e0a2
DanielEvaristo <evaristo.camargo.daniel@gmail.com>
2024-11-17 17:18:54 -0600

    Fix Action

--- actions/employees/Update.ts
+++ actions/employees/Update.ts
@@ -5,19 +5,22 @@ import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
 
 export default async function UpdateEmployee(employeeId: string, formData: FormData){
-    formData.delete("$ACTION_REF_0")
-    formData.delete("$ACTION_0:1")
-    formData.delete("$ACTION_0:0")
+    const cleanData = new FormData();
+    for (const [key, value] of formData.entries()){
+        if(!key.startsWith("$")){
+            cleanData.append(key, value)
+        }
+    }
 
     const response  = await fetch(`${API_URL}/employees/${employeeId}`,{
         method:"PATCH",
         headers:{
             ...authHeaders(),
         },
-        body: formData,
+        body: cleanData,
     })
     if(response.status === 200) revalidateTag("dashboard:employees")
-    if(response.status === 200) revalidateTag(`dashboard:employees/${employeeId}`)
+    if(response.status === 200) revalidateTag(`dashboard:employees:${employeeId}`)
 
     return;
 }
--- app/dashboard/employees/_components/FormCreateEmployee.tsx
+++ app/dashboard/employees/_components/FormCreateEmployee.tsx
@@ -1,16 +1,25 @@
 
 import createEmployee from "@/actions/employees/Create";
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
 import { Button, Input } from "@nextui-org/react";
+import SelectLocations from "./SelectLocation";
 
-export default function FormCreateEmployee(){
-   
+export default async function FormCreateEmployee(){
+    const responseLocations = await fetch(`${API_URL}/locations`,{
+        headers:{
+            ...authHeaders(),
+        }
+    })
+    const locations = await responseLocations.json()
     return (
         <form action={createEmployee} className="flex flex-col gap-2 p-8 rounded-md m-2 bg-orange-500 h-fit">
             <Input isRequired={true} label="Nombre" name="employeeName" placeholder="Marco"/>
             <Input isRequired={true} label="Apellidos" name="employeeLastName" placeholder="Aurelio"/>
-            <Input isRequired={true} label="Correo electrónico" name="employeeEmail" placeholder="432442342"/>
-            <Input isRequired={true} label="Número de teléfono" name="employeePhoneNumber" placeholder="marco@gmai.com"/>
+            <Input isRequired={true} label="Correo electrónico" name="employeeEmail" placeholder="marco@gmai.com"/>
+            <Input isRequired={true} label="Número de teléfono" name="employeePhoneNumber" placeholder="4423696264"/>
             <Input type="file" name="employeePhoto" />  
+            <SelectLocations stores={locations}/>
             <Button type="submit" color="primary">Agregar empleado</Button>
         </form>
     )
